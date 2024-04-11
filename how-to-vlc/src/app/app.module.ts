2e38247cd03dd9865150bfceb708587b0524c527
WhatAnAngel <angelilai.al@hotmail.com>
2024-04-11 08:53:40 +0200

    Clonado el repositorio principal del Front

---
+++ how-to-vlc/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RecursosComponent } from './pages/recursos/recursos.component';
+import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { EventosComponent } from './pages/eventos/eventos.component';
+import { MapaComponent } from './pages/mapa/mapa.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecursosComponent,
+    LoginComponent,
+    FooterComponent,
+    DashboardComponent,
+    EventosComponent,
+    MapaComponent,
+    PerfilComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
