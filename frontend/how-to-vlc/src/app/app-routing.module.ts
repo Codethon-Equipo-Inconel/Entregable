3de65a9f78176feac5c86f8cc2194e089b1147b1
WhatAnAngel <angelilai.al@hotmail.com>
2024-05-09 18:53:29 +0200

    Update Sprint 3

--- frontend/how-to-vlc/src/app/app-routing.module.ts
+++ frontend/how-to-vlc/src/app/app-routing.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecursosComponent } from './pages/recursos/recursos.component';
+import { RecursoComponent } from './pages/recursos/componentes/recurso/recurso.component';
 import { EventosComponent } from './pages/eventos/eventos.component';
 import { MapaComponent } from './pages/mapa/mapa.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [{
+  path: 'login',
+  component: LoginComponent
+},
+{
+  path: 'register',
+  component: RegisterComponent
+},
+{
   path: 'dashboard',
   component: DashboardComponent
 },
 {
   path: 'recursos',
   component: RecursosComponent
+},
+{
+  path: 'recurso',
+  component: RecursoComponent
 },
 {
   path: 'eventos',
@@ -27,9 +42,11 @@ const routes: Routes = [{
   component: PerfilComponent
 },
 {
-  path: '', redirectTo: 'dashboard', pathMatch: 'full'
+  path: '', redirectTo: 'login', pathMatch: 'full'
 }
 ];
+
+export const routing = RouterModule.forRoot(routes);
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/how-to-vlc/src/app/app.module.ts
+++ frontend/how-to-vlc/src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RecursosComponent } from './pages/recursos/recursos.component';
-import { LoginComponent } from './components/login/login.component';
+import { RecursoComponent } from './pages/recursos/componentes/recurso/recurso.component';
+import { LoginComponent } from './pages/login/login.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { EventosComponent } from './pages/eventos/eventos.component';
 import { MapaComponent } from './pages/mapa/mapa.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { GalleryComponent } from './pages/eventos/componentes/gallery/gallery.component';
+import { LocalidadesServicesService } from './services/localidades-services.service';
+import { RegisterComponent } from './pages/register/register.component';
+import { GradientComponent } from './components/gradient/gradient.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecursosComponent,
+    RecursoComponent,
     LoginComponent,
     FooterComponent,
     DashboardComponent,
@@ -25,12 +31,15 @@ import { GalleryComponent } from './pages/eventos/componentes/gallery/gallery.co
     MapaComponent,
     PerfilComponent,
     GalleryComponent,
+    RegisterComponent,
+    GradientComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [LocalidadesServicesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
